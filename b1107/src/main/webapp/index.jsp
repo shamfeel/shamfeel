5fc9c7d19d9264eee6b64980271de5e064da9166
shamfeel <shamfeel@gmail.com>
2018-10-24 12:38:57 +0530

    all commit

---
+++ b1107/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1>Maven First project with jenkins</h1><h2>Hello World!</h2>
+</body>
+</html>
---
+++ examples/src/main/java/com/shamfeel/tk/App.java
@@ -0,0 +1,14 @@
+package com.shamfeel.tk
+
+import org.testing.annotations.*;
+
+public class TestNGTest{
+
+        
+      @Test
+	Public void testcaseone()
+{
+      System.out.println("This my testingby shamfeel");
+}
+   
+}
